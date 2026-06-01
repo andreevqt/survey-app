100630f2f488e2a720f541ec115d003440a46af4
andreevxdr <qprquo@gmail.com>
2026-06-01 03:11:50 +0300

    feat(frontend): fix dashboard shell's inner padding

--- frontend/src/layouts/DashboardShell/DashboardShell.tsx
+++ frontend/src/layouts/DashboardShell/DashboardShell.tsx
@@ -23,7 +23,7 @@ export function DashboardShell() {
         <Sidebar />
         <div className="flex-1 flex flex-col min-w-0">
           <TopBar />
-          <main className="flex-1 px-8 py-7 max-w-7xl mx-auto w-full">
+          <main className="flex-1 py-7 max-w-7xl mx-auto w-full">
             <Outlet />
           </main>
         </div>
